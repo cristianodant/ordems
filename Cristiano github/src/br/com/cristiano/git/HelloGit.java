0a54691537ad04fe88dc1a373fb703261f631872
Aluno <Aluno@192.168.68.180>
2019-08-07 20:38:09 -0300

    Foi dado inicio no projeto

--- Cristiano github/src/br/com/cristiano/git/HelloGit.java
+++ Cristiano github/src/br/com/cristiano/git/HelloGit.java
@@ -11,7 +11,8 @@ public class HelloGit {
 
 	
 	public static void main(String[] args) {
-		System.out.println ("Hello Git");
+		System.out.println ("ordems");
+		System.out.println ("Iniciando Projeto Ordems");
 
 	}
 
